e612d793058d69430c0fc176bb3dc118b78edb8d
Florian Alphonzair <florian.alphonzair@etudiant.univ-rennes1.fr>
2024-01-22 20:50:49 +0100

    Commit progression.

--- code/roman-numerals/src/main/java/fr/istic/vv/RomanNumeraUtils.java
+++ code/roman-numerals/src/main/java/fr/istic/vv/RomanNumeraUtils.java
@@ -1,10 +1,41 @@
 package fr.istic.vv;
 
 public class RomanNumeraUtils {
+
+        private enum RomanNumeral {
+                I('I'), V('V'), X('X'), C('C'), L('L'), D('D'), M('M');
+
+                private final char i;
+
+                RomanNumeral(char i) {
+                        this.i = i;
+                }
+
+                public char getRomanNumeral(){
+                        return this.i;
+                }
+        }
     
         public static boolean isValidRomanNumeral(String value) { return false; }
     
-        public static int parseRomanNumeral(String numeral) { return 0; }
+        public static int parseRomanNumeral(String numeral) {
+                int result = 0;
+
+                if (numeral == null){
+                        throw new IllegalArgumentException();
+                }
+                if (numeral.isBlank() || numeral.isEmpty()){
+                        return 0;
+                }
+                numeral = numeral.toUpperCase();
+
+                for(int i=0 ; i<numeral.length(); i++){
+                        int tmp = 0;
+                        switch (numeral.charAt(i)){
+                        }
+                }
+                return 0;
+        }
     
         public static String toRomanNumeral(int number) { return ""; }
     
