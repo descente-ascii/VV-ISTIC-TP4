1ebcf7233a14effc02d807c237199c8ceee1bbd4
Florian Alphonzair <florian.alphonzair@etudiant.univ-rennes1.fr>
2024-01-23 15:13:30 +0100

    Fix symbolValue errors.

--- code/roman-numerals/src/main/java/fr/istic/vv/RomanNumeraUtils.java
+++ code/roman-numerals/src/main/java/fr/istic/vv/RomanNumeraUtils.java
@@ -81,10 +81,11 @@ public class RomanNumeraUtils {
                 // can't use switch to compile
                 if (s == 'I') return 1;
                 else if (s == 'V') return 5;
-                else if (s == 'X') return 5;
-                else if (s == 'L') return 5;
-                else if (s == 'C') return 5;
-                else if (s == 'D') return 5;
+                else if (s == 'X') return 10;
+                else if (s == 'L') return 50;
+                else if (s == 'C') return 100;
+                else if (s == 'D') return 500;
+                else if (s == 'M') return 1000;
                 else return 0;
         }
 
